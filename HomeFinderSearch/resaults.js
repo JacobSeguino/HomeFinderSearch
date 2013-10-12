80f77e49504c0838268fa58f0206378fc9b8b3fb
Jacob-seguino <jacob.seguino@gmail.com>
2013-10-12 17:26:02 -0500

    ne chagnes

---
+++ HomeFinderSearch/resaults.js
@@ -0,0 +1,17 @@
+function showResponse(response) {
+    var responseString = JSON.stringify(response, '', 2);
+    document.getElementById('response').innerHTML += responseString;
+}
+function makeRequest() {
+        var request = gapi.client.urlshortener.url.get({
+          'shortUrl': 'http://services.homefinder.com'
+        });
+        request.execute(function(response) {
+          appendResults(response.longUrl);
+        });
+      }
+
+      function load() {
+        gapi.client.setApiKey('YOUR API KEY');
+        gapi.client.load('urlshortener', 'v1', makeRequest);
+      }
---
+++ HomeFinderSearch/search.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$('#opt1').click(function(){
+		$('#pets').toggleClass('pets');
+	});
+});
+var baseUrl = "http://services.homefinder.com";
+var listingSearch = "/listingServices/search";
+var apiKey = "apikey=n6vyvm6ktkdh44w43zrqwpb7";
