d7b3fa2dfe3e8825886ecea379c3876ea20d8eab
Jacob-seguino <jacob.seguino@gmail.com>
2013-10-12 20:39:42 -0500

    still can't figure out gitJSON, but the url looks nice

--- HomeFinderSearch/search.js
+++ HomeFinderSearch/search.js
@@ -2,18 +2,25 @@ $(document).ready(function(){
 	
 	var baseUrl = "http://services.homefinder.com";
 	var listingSearch = "/listingServices/search";
-	var apiKey = "apikey=n6vyvm6ktkdh44w43zrqwpb7";
+	var apiKey = "&apikey=n6vyvm6ktkdh44w43zrqwpb7";
+	var apiUrl = baseUrl + listingSearch;
 	
 	$('#searchButon').click(function(){
 		var location = $('input[name=area]').val();
-		var apiUrl= baseUrl + listingSearch + location;
-		$('#test').append('<div class="test">'+apiUrl+'</div>');
+		apiUrl+=location;
+		if($('input[name=areCatsAllowed]').val()='true'){
+			apiUrl+="&areCatsAllowed=true";
+		}
+		if($'input[name=areDogsAllowed]').val()="true"){
+			apiUrl+="areDogsAllowed=true";
+		}
+		
+		apiUrl= apiUrl + apiKey;
+		
+		$.getJSON(apiUrl, function(jd) {
+	        $('#test').html('<p> Name: ' + jd.name + '</p>');
+	    });	
 	});
-})
-
-
-
 	
 
-// var json = JSON.parse(response);
-// console.log(json);
+})
