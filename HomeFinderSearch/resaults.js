f9892fd7ba99ecb134afc135328e3b598b4bbd34
Jacob-seguino <jacob.seguino@gmail.com>
2013-10-12 19:56:58 -0500

    changes

--- HomeFinderSearch/resaults.js
+++ HomeFinderSearch/resaults.js
@@ -3,15 +3,15 @@ function showResponse(response) {
     document.getElementById('response').innerHTML += responseString;
 }
 function makeRequest() {
-        var request = gapi.client.urlshortener.url.get({
-          'shortUrl': 'http://services.homefinder.com'
-        });
-        request.execute(function(response) {
-          appendResults(response.longUrl);
-        });
-      }
+    var request = gapi.client.urlshortener.url.get({
+      'shortUrl': 'http://services.homefinder.com'
+    });
+    request.execute(function(response) {
+      appendResults(response.longUrl);
+    });
+}
 
-      function load() {
-        gapi.client.setApiKey('YOUR API KEY');
-        gapi.client.load('urlshortener', 'v1', makeRequest);
-      }
+function load() {
+    gapi.client.setApiKey('YOUR API KEY');
+    gapi.client.load('urlshortener', 'v1', makeRequest);
+}
--- HomeFinderSearch/search.js
+++ HomeFinderSearch/search.js
@@ -1,8 +1,19 @@
 $(document).ready(function(){
-	$('#opt1').click(function(){
-		$('#pets').toggleClass('pets');
+	
+	var baseUrl = "http://services.homefinder.com";
+	var listingSearch = "/listingServices/search";
+	var apiKey = "apikey=n6vyvm6ktkdh44w43zrqwpb7";
+	
+	$('#searchButon').click(function(){
+		var location = $('input[name=area]').val();
+		var apiUrl= baseUrl + listingSearch + location;
+		$('#test').append('<div class="test">'+apiUrl+'</div>');
 	});
-});
-var baseUrl = "http://services.homefinder.com";
-var listingSearch = "/listingServices/search";
-var apiKey = "apikey=n6vyvm6ktkdh44w43zrqwpb7";
+})
+
+
+
+	
+
+// var json = JSON.parse(response);
+// console.log(json);
